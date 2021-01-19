bda038f0354965c6ebb91046b3dcbbb923480137
Luca Cavretti <cavretiluca@live.it>
2021-01-19 19:12:34 +0100

    Swap with Constructor

--- dipendenti.php
+++ dipendenti.php
@@ -1,50 +1,109 @@
 <?php 
 
 class Persona {
-    public $nome;
-    public $cognome;
-    public $codice_fiscale;
+    private $nome;
+    private $cognome;
+    private $codice_fiscale;
 
+    public function __construct($nome, $cognome, $codice_fiscale){
+        $this->nome = $nome;
+        $this->cognome = $cognome;
+        $this->codice_fiscale = $codice_fiscale;
+    }
 
     public function to_string(){
-        echo 'Nome: '.$this->nome.'<br> Cognome: '.$this->cognome.'<br> Codice Fiscale: '.$this->codice_fiscale;
+        return 'Nome: '.$this->nome.'<br> Cognome: '.$this->cognome.'<br> Codice Fiscale: '.$this->codice_fiscale;
     }
 }
 
 class Impiegato extends Persona {
 
-    public $codice_impiegato;
-    public $compenso;
+    private $codice_impiegato;
+    private $compenso;
 
-    public function parentToString() {
-        parent::to_string();
+    public function __construct($nome, $cognome, $codice_fiscale, $codice_impiegato, $compenso){
+        
+        parent::__construct($nome, $cognome, $codice_fiscale);
+
+        $this->codice_impiegato = $codice_impiegato;
+        $this->compenso = $compenso;
+    }
+
+    public function to_string() {
+        $data = parent::to_string();
+        $data .= '<br>Codice Impiegato: '.$this->codice_impiegato.'<br>Compenso: '.$this->compenso;
+        return $data;
     }
     
 }
 
 class ImpiegatoSalariato extends Impiegato{
-    public $giorni_lavorati;
-    public $compenso_giornaliero;
+    private $giorni_lavorati;
+    private $compenso_giornaliero;
+
+    public function __construct($nome, $cognome, $codice_fiscale, $codice_impiegato, $compenso, $giorni_lavorati, $compenso_giornaliero){
+        
+        parent::__construct($nome, $cognome, $codice_fiscale, $codice_impiegato, $compenso);
+
+        $this->giorni_lavorati = $giorni_lavorati;
+        $this->compenso_giornaliero = $compenso_giornaliero;
+    }
 
     public function calcola_compenso(){
-        return $this->giorni_lavorati*$this->compenso_giornaliero;
+        $compensoS = $this->giorni_lavorati*$this->compenso_giornaliero;
+        return $compensoS;
+    }
+
+    public function to_string() {
+        $data = parent::to_string();
+        $data .= '<br>Bonus Compenso Salariato: '.self::calcola_compenso();
+        return $data;
     }
 }
 
 class ImpiegatoAOre extends Impiegato{
-    public $ore_lavorate;
-    public $compenso_orario;
+    private $ore_lavorate;
+    private $compenso_orario;
+
+    public function __construct($nome, $cognome, $codice_fiscale, $codice_impiegato, $compenso, $ore_lavorate, $compenso_orario){
+
+        parent::__construct($nome, $cognome, $codice_fiscale, $codice_impiegato, $compenso);
+
+        $this->ore_lavorate = $ore_lavorate;
+        $this->compenso_orario = $compenso_orario;
+    }
 
     public function calcola_compenso(){
-        return $this->ore_lavorate*$this->compenso_orario;
+        $compensoOre = $this->ore_lavorate*$this->compenso_orario;
+        return $compensoOre;
+    }
+
+    public function to_string(){
+        $data = parent::to_string();
+        $data.= '<br>Bonus Impiegato a Ore: '.self::calcola_compenso();
+        return $data;
     }
 }
 
 class ImpiegatoSuCommissione extends Impiegato{
     use Progetto;
 
+    public function __construct($nome, $cognome, $codice_fiscale, $codice_impiegato, $compenso, $nome_progetto, $commissione_progetto){
+
+        parent::__construct($nome, $cognome, $codice_fiscale, $codice_impiegato, $compenso);
+
+        $this->nome_progetto = $nome_progetto;
+        $this->commissione_progetto = $commissione_progetto;
+    }
+
     public function calcola_compenso(){
         return $this->commissione_progetto;
+    }
+
+    public function to_string(){
+        $data = parent::to_string();
+        $data .= '<br>Nome Progetto: '.$this->nome_progetto.'<br> Commissione Progetto'.self::calcola_compenso();
+        return $data;
     }
 }
 
@@ -53,8 +112,3 @@ trait Progetto {
     public $commissione_progetto;
 }
 
-$dipendente1 = new ImpiegatoSuCommissione();
-$dipendente1->nome = 'Luca';
-$dipendente1->cognome = 'Cavretti';
-$dipendente1->codice_fiscale = 'CVRLCU94M01D548W';
-$dipendente1->commissione_progetto = 10;
--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
     require_once ('dipendenti.php');
+    
+    $dipendente1 = new ImpiegatoSuCommissione('Luca', 'Cavretti', 'CODICEFISCALE', '00000', 1000,'Ciao',500);
 
-    echo $dipendente1->to_string().'<br>Compenso: '. $dipendente1->calcola_compenso().'€';
+    echo $dipendente1->to_string();
+
 
 ?>
